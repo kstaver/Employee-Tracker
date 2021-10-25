d1644a9a0d40b5c2234924a9b22fdb4c2f4f8bf7
Kendra Staver <staverkendra@gmail.com>
2021-10-24 18:23:34 -0700

    base schema created

---
+++ assets/sql/employees.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+CREATE TABLE department(
+
+);
+CREATE TABLE role(
+
+);
+CREATE TABLE employee(
+
+);
+USE employees;
+INSERT INTO department;
+INSERT INTO role;
+INSERT INTO employee;

7c96e01b629a5a234d7531a46a3cbb9a73780f20
Kendra Staver <staverkendra@gmail.com>
2021-10-25 18:56:11 -0700

    completed all teh view functions

--- index.js
+++ index.js
@@ -84,22 +84,72 @@ function prompt(){
 
 // View all employees option
 function viewAllEmployees(){
-
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name
+    AS department, role.salary, CONCAT(manager.first_name,'',manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;`
+    connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW ALL EMPLOYEES');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
 }
 
 // View employees by department
 function viewByDepartment(){
-
+    const query = `SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role.id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;`
+    connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY DEPARTMENT');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
 }
 
 // View employees by their manager's name
 function viewByManager(){
-
+   const query = `SELECT CONCAT(manager.first_name,'',manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+   FROM employee
+   LEFT JOIN employee manager ON manager.id = employee,manager_id
+   INNE JOIN department ON (department.id = role.department_id)
+   ORDER BY manager;`;
+   connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY MANAGER');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
 }
 
 // View employees 
 function viewAllRoles(){
-
+    const query = `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;`;
+    connection.query(query, (err, res) => {
+        if(err) throw err;
+        console.log('\n');
+        console.log('VIEW EMPLOYEE BY MANAGER');
+        console.log('\n');
+        console.table(res);
+        prompt();
+    });
 }
 
 // Add an employee to the database

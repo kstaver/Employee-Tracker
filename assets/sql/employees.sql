1d54b1f828e1a37d28c02a6b4ddc61ab4a612315
Kendra Staver <staverkendra@gmail.com>
2021-11-01 22:13:07 -0700

    added a select from for the department

--- assets/sql/employees.sql
+++ assets/sql/employees.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
 USE employees;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'crazycatlady';
 CREATE TABLE department(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL

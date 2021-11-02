c1b8d6897c6407f69676f4ee16803b375a4e6826
Kendra Staver <staverkendra@gmail.com>
2021-11-01 22:13:36 -0700

    same as before

--- assets/sql/seed.sql
+++ assets/sql/seed.sql
@@ -1,6 +1,7 @@
 SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
 SELECT role.id, role.title FROM role ORDER BY role.id;
 SELECT * FROM employee;
+SELECT * FROM department;
 
 SELECT department.id, department.name FROM department ORDER BY department.id;
 

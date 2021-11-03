e4ff446bf4617244b1a789b79bae7508378c29c9
Kendra Staver <staverkendra@gmail.com>
2021-11-03 14:55:12 -0700

    removed manager references to simplify the code

--- assets/sql/employees.sql
+++ assets/sql/employees.sql
@@ -3,33 +3,24 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department(
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
-CREATE TABLE manager(
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    full_name VARCHAR(30)
-);
 CREATE TABLE role(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL UNSIGNED NOT NULL,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) 
-    REFERENCES department(id) 
-    ON DELETE SET NULL
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+--  ON DELETE SET NULL
 );
 CREATE TABLE employees(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id) 
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) 
-    REFERENCES manager(id) 
-    ON DELETE SET NULL
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id),
+--  ON DELETE SET NULL
 );

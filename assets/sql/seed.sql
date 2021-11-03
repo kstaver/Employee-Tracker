8444c331aefeee4ec39367038f1a44970c92120a
Kendra Staver <staverkendra@gmail.com>
2021-11-03 14:54:35 -0700

    removed delete options in case those are bugged

--- assets/sql/seed.sql
+++ assets/sql/seed.sql
@@ -1,34 +1,32 @@
-SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
-SELECT role.id, role.title FROM role ORDER BY role.id;
-SELECT * FROM employee;
-SELECT * FROM department;
-
-SELECT department.id, department.name FROM department ORDER BY department.id;
-
-SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY department.name;
-    
-SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
-  FROM employee
-  LEFT JOIN employee manager on manager.id = employee.manager_id
-  INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
-  INNER JOIN department ON (department.id = role.department_id)
-  ORDER BY manager;
-  
-SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY role.title;
-
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-  FROM employee
-  LEFT JOIN employee manager on manager.id = employee.manager_id
-  INNER JOIN role ON (role.id = employee.role_id)
-  INNER JOIN department ON (department.id = role.department_id)
-  ORDER BY employee.id;
-  
-SELECT first_name, last_name, role_id FROM employee 	WHERE employee.id = 4;
+INSERT INTO department(name)
+VALUES
+    ('Engineering'),
+    ('Quality Assurance'),
+    ('Network Engineering'),
+    ('Executive');
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Lead Engineer', 200000, 1),
+    ('Engineer', 150000, 1),
+    ('Quality Assurance Lead', 100000, 2),
+    ('Quality Assurance', 70000, 2),
+    ('Network Engineer Lead', 150000, 3),
+    ('Network Engineer', 90000, 3),
+    ('CISO', 250000, 4),
+    ('Director of Information Security', 200000, 4);
+INSERT INTO manager(full_name)
+VALUES
+  ('Henrietta Crabgrass'),
+  ('Hendrick Crabgrass'),
+  ('Hendersun Crabgrass'),
+  ('Henderson Crabgrass');
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    (Henrietta, Crabgrass, 1, NULL)
+    (Henry, Crabgrass, 2, 1)
+    (Hendrick, Crabgrass, 3, NULL)
+    (Henlea, Crabgrass, 4, 3)
+    (Hendersun, Crabgrass, 5, NULL)
+    (Hendrikje, Crabgrass, 6, 5)
+    (Henderson, Crabgrass, 7, NULL)
+    (Henka, Crabgrass, 8, 7);

c1bde6bf26cc87a7effc2e7ff48c937fd6bb6c9b
Kendra Staver <staverkendra@gmail.com>
2021-11-03 18:55:46 -0700

    tried something. did not work. rewriting code

--- assets/sql/employees.sql
+++ assets/sql/employees.sql
@@ -3,24 +3,32 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE manager(
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    full_name VARCHAR(30)
 );
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY(id),
---  ON DELETE SET NULL
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id) 
+    ON DELETE SET NULL
 );
 CREATE TABLE employees(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY(id),
---  ON DELETE SET NULL
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id) 
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) 
+    REFERENCES manager(id) 
+    ON DELETE SET NULL
 );
--- assets/sql/seed.sql
+++ assets/sql/seed.sql
@@ -1,11 +1,11 @@
 INSERT INTO department(name)
-VALUE
+VALUES
     ('Engineering'),
     ('Quality Assurance'),
     ('Network Engineering'),
     ('Executive');
 INSERT INTO role (title, salary, department_id)
-VALUE
+VALUES
     ('Lead Engineer', 200000, 1),
     ('Engineer', 150000, 1),
     ('Quality Assurance Lead', 100000, 2),
@@ -15,13 +15,13 @@ VALUE
     ('CISO', 250000, 4),
     ('Director of Information Security', 200000, 4);
 INSERT INTO manager(full_name)
-VALUE
+VALUES
   ('Henrietta Crabgrass'),
   ('Hendrick Crabgrass'),
   ('Hendersun Crabgrass'),
   ('Henderson Crabgrass');
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE
+VALUES
     (Henrietta, Crabgrass, 1, NULL)
     (Henry, Crabgrass, 2, 1)
     (Hendrick, Crabgrass, 3, NULL)

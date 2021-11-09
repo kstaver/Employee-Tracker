009a97470a6363437b1be4af4222acf340be0ef5
Kendra Staver <staverkendra@gmail.com>
2021-11-08 20:07:02 -0800

    more bugs and sql pass not working

--- utils/connectUtil.js
+++ utils/connectUtil.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const {async} = require('rxjs');
 
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'new-password',
+    password: 'password',
     database: 'techBusiness',
     waitForConnections: true,
     connectionLimit: 10,

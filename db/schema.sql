d5842e1da5e3ea6fd8cadde63dd6206c9d9b1875
Kendra Staver <staverkendra@gmail.com>
2021-11-09 12:40:20 -0800

    added constraints

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE role(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
-    department_id INTEGER NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department
     FOREIGN KEY (department_id) 
     REFERENCES department(id) 
     ON DELETE SET NULL
@@ -19,7 +20,8 @@ CREATE TABLE employee(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
+    role_id INTEGER,
+    CONSTRAINT fk_role
     FOREIGN KEY (role_id)
     REFERENCES role(id) 
     ON DELETE SET NULL,

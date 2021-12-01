fe5420e568f07341aef8ad51c580f9086d7d4cca
Kendra Staver <staverkendra@gmail.com>
2021-12-01 12:43:38 -0800

    added base async functions

--- inquries.js
+++ inquries.js
@@ -1,1 +1,37 @@
 // For async functions
+const db = require('../../db');
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+
+// View all employees
+async function viewEmployees(table){
+
+
+}
+
+// View all managers
+async function getAllManagers(){
+
+}
+
+// Add an employee
+async function addEmployee(table){
+
+}
+
+// Add a department
+async function addDepartment(table){
+
+}
+
+// Add a role
+async function addRole(table){
+
+}
+
+// Update an employee
+async function updateEmployee(updateValue){
+
+}
+
+module.exports = { viewEmployees, getAllManagers, addEmployee, addDepartment, addRole, updateEmployee };

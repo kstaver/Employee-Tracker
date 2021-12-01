67383aaa60ce37e51d629876938134f1424c1cff
Kendra Staver <staverkendra@gmail.com>
2021-12-01 14:17:33 -0800

    addeded allManagers & added all options

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const mainMenu = async () => {
                 "View all employees",
                 "View all departments",
                 "View all roles",
+                "View all Managers",
                 new inquirer.Separator(),
                 "Add an employee",
                 "Add a department",
@@ -37,10 +38,71 @@ const mainMenu = async () => {
             ],
         });
         console.log('\n');
-        if(){
-            
+        if(
+            choices.options === "View all employees" ||
+            choices.options === "View all departments" ||
+            choices.options === "View all roles" ||
+            choices.options === "View all Managers"
+        ){
+            let table;
+            switch (choices.options){
+                case "View all employees":{
+                    table = "employee";
+                    break;
+                }
+                case "View all department":{
+                    table = "department";
+                    break;
+                }
+                case "View all roles":{
+                    table = "role";
+                    break;
+                }
+                case "View all Managers":{
+                    table = "manager";
+                }
+                default:{
+                    throw new Error("Invalid choice");
+                }
         }
-    } catch(e){
+        const results = await viewEmployees(table);
+        console.table(results);
+        await mainMenu();
+        } else if (
+            choices.options === "Add an employee" ||
+            choices.options === "Add a department" ||
+            choices.options === "Add a role"
+        ){
+            let table;
+            switch (choices.options){
+                case "Add an employee":{
+                    table = addEmployee();
+                    break;
+                }
+                case "Add a department":{
+                    table = addDepartment();
+                    break;
+                }
+                case "Add a role":{
+                    table = addRole();
+                    break;
+                }
+                default:{
+                    throw new Error("Invalid choice.");
+                }
+            }
+            await addEmployee(table);
+            await addDepartment(table);
+            await addRole(table);
+            await mainMenu();
+        } else if (choices.options === "Update an employee"){
+            let updateValue = "employee";
+            await updateEmployee(updateValue);
+            await mainMenu();
+        } else if (choices.options === "Exit"){
+            process.exit();
+        }
+    } catch (e){
         console.log(err);
     }
 };

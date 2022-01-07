23924b12585a520f54ffe67f350923025da3b07a
kstaver <staverkendra@gmail.com>
2022-01-06 18:31:59 -0800

    changed auto increment to identify

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE techbusiness;
 USE techbusiness;
 
 CREATE TABLE department(
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+    id INTEGER IDENTITY NOT NULL PRIMARY KEY,
+    name VARCHAR(30)
 );
 CREATE TABLE role(
-    id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER IDENTITY PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
+    department_id INT NOT NULL,
 	  INDEX dep_ind (department_id),
 	  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INT IDENTITY NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
+  role_id INT NOT NULL,
   INDEX role_ind (role_id),
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
+  manager_id INT  ,
   INDEX man_ind (manager_id),
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
